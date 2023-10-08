78b86fba56924cf2bc64f2cfc2d3af817fb6b4c8
alakir <dmitrij.balymoff@yandex.ru>
2023-10-08 21:11:10 +0300

    first and second module

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,16 @@
 let calculator = {
-  // ваш код
+  a: 0,
+  b: 0,
+  read: function (a, b) {
+    this.a = a;
+    this.b = b;
+  },
+  sum: function (a, b) {
+    return this.a + this.b;
+  },
+  mul: function (a, b) {
+    return this.a * this.b;
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
